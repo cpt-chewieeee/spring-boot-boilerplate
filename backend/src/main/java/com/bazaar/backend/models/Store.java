03d7d5c865f4cf51cd67f937f042e10421c1395e
cpt-chewieeee <hzhan016@gmail.com>
2024-08-03 00:58:39 -0700

    cleaning up

--- backend/src/main/java/com/bazaar/backend/models/Store.java
+++ backend/src/main/java/com/bazaar/backend/models/Store.java
@@ -1,20 +1,16 @@
 package com.bazaar.backend.models;
 
-import java.sql.Date;
-
-import org.hibernate.annotations.CreationTimestamp;
-
-import io.micrometer.common.lang.NonNull;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "STORES")
 public class Store {
@@ -22,21 +18,11 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
 
-    @Column(name = "name", nullable = false)
-  
-    private String Name;
+   
+    private String name;
     
-    @Column(name = "user_id")
-    // @ManyToOne(cascade = CascadeType.DETACH)
-    @JoinColumn(name = "users.id")
-    private int UserId;
 
-    @NonNull
-    @Column(name = "create_date", updatable = false)
-    @CreationTimestamp
-    private Date CreateDate;
-
-
-    @Column(name = "last_update")
-    private Date LastUpdate;
-}
+    public Store(String name) {
+      this.name = name;
+    }
+} 
